63fb422075202ab321751bb61451ce374e01b4dc
LittleCoinCoin <eliott.jacopin@laposte.net>
2025-06-12 17:32:44 +0900

    [Update] Public API
    **Major**:
    - update the `__init__` to expose the `SchemaCache` and `SchemaFetcher` classes
    
    **Minor**:
    - Removed the `requirements.txt` as the dependencies are already in the `pyproject.toml`

--- hatch_validator/__init__.py
+++ hatch_validator/__init__.py
@@ -8,8 +8,11 @@ __version__ = "0.3.2"
 
 from .package_validator import HatchPackageValidator, PackageValidationError
 from .dependency_resolver import DependencyResolver, DependencyResolutionError
+from .schema_fetcher import SchemaFetcher
+from .schema_cache import SchemaCache
+
 from .schemas_retriever import (
-    SchemaRetriever, 
+    SchemaRetriever,
     get_package_schema, 
     get_registry_schema
 )
@@ -20,6 +23,8 @@ __all__ = [
     'DependencyResolver', 
     'DependencyResolutionError',
     'SchemaRetriever',
+    'SchemaFetcher',
+    'SchemaCache',
     'get_package_schema',
     'get_registry_schema',
 ]
